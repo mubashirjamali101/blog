17958b54287fd620649a372436309eb7e80351dc
Mubashir <mubashirjamali101@gmail.com>
2020-04-27 23:33:45 +0500

    modified base, new entry, readmore or full blog and home page view

--- entries/urls.py
+++ entries/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from .views import HomeView, EntryView, CreateEntryView
+from .views import HomeView, EntryView, CreateEntryView #importing all the views
 
 urlpatterns = [
-    path('', HomeView.as_view(), name="blog-home"),
-    path('entry/<int:pk>/',EntryView.as_view(), name = "entry-detail"),
-    path('create_entry', CreateEntryView.as_view(), name = "create_entry")
+    path('', HomeView.as_view(), name="blog-home"),#home page url
+    path('entry/<int:pk>/',EntryView.as_view(), name = "entry-detail"),#readmore or full blog view url
+    path('create_entry', CreateEntryView.as_view(success_url='/'), name = "create_entry")# create new post url
 ]
--- entries/views.py
+++ entries/views.py
@@ -6,13 +6,20 @@ class HomeView(ListView):
     model = Entry
     template_name = 'entries/index.html'
     context_object_name = "blog_entries"
+    ordering = ['-entry_date'] #sorting by the item date
+    paginate_by = 3 #three items per page
 
-
+###readmore or full blog view
 class EntryView(DetailView):
     model = Entry
     template_name = 'entries/entry_detail.html'
 
+###create new post view
 class CreateEntryView(CreateView):
     model = Entry
     template_name = 'entries/create_entry.html'
-    fields = ['title', 'text']
+    fields = ['entry_title', 'entry_text', 'entry_image_url']
+
+    def form_valid(self, form):
+        form.instance.entry_author = self.request.user
+        return super().form_valid(form)
